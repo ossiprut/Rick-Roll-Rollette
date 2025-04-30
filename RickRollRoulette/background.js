c1c32b0460044b94322f6b8d085256f045240069
ossiprut <129540005+ossiprut@users.noreply.github.com>
2025-04-30 19:20:42 +0200

    Delete RickRollRoulette directory

--- RickRollRoulette/background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onCreated.addListener((tab) => {
-    // Generate a random number between 1 and 6
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
-  
-    // Check if the random number is 1 (1/6 chance)
-    if (randomNumber === 1) {
-      chrome.tabs.update(tab.id, { url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" });
-    }
-  });
